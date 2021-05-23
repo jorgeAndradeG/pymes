076b1a1afc2441a0940734f103e134a3251a5241
jorgeAndradeG <gomezjorgeandrade@gmail.com>
2021-05-23 11:07:55 -0400

    agregar imagen en el perfil

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Validator;
 
 class PerfilController extends Controller
 {
@@ -71,7 +73,45 @@ class PerfilController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $usuario = User::findOrFail($id); //BUSCAMOS AL USUARIO EN LA BD CON SU ID
+        //RESCATAMOS LOS DATOS DEL FORMULARIO Y LOS ASIGNAMOS EN LOS CAMPOS CORRESPONDIENTES
+        $usuario->name = $request['name']; 
+        $usuario->direccion = $request['direccion'];
+        $usuario->telefono = $request['telefono'];
+        $usuario->facebook = $request['facebook'];
+        $usuario->instagram = $request['instagram'];
+
+
+
+        //PARA SUBIR LAS IMÁGENES
+        
+        $file = $request['file'];//RESCATAMOS LA IMAGEN DEL FORMULARIO
+        $nombre = $file->getClientOriginalName();
+        $formato = explode(".",$nombre);
+        $formato = end($formato);
+
+        if (strtolower($formato) != "jpg" && strtolower($formato) != "jpeg" && strtolower($formato) != "png" )
+        {
+            //CUANDO NO ES IMAGEN
+            $user = Auth::user(); //OBTENEMOS AL USUARIO QUE ESTÁ LOGGEADO.
+            return view('pymes.perfil.editar-perfil')->with(['usuario' => $user, 'msg' => $file]); //RETORNAMOS LA VISTA Y EL MENSAJE DE ERROR
+        } else if(strtolower($formato) == "jpg" || strtolower($formato) == "jpeg" || strtolower($formato) == "png") //CUANDO SI ES UNA IMAGEN
+        {
+        
+            $fecha = getdate();
+            $fechaimg = strval($fecha["year"]) . strval($fecha["mon"]) . strval($fecha["mday"]) . strval($fecha["hours"]) . strval($fecha["minutes"]) . strval($fecha["seconds"]) . "_";
+
+            Image::make($file)->resize(600,400)->save('img/' . $fechaimg . $nombre);
+        };
+
+        $usuario->imagen_perfil = '/img/' . $fechaimg . $nombre;
+
+
+        //GUARDAMOS LOS DATOS DE CADA USUARIO
+        $usuario->save();
+
+        return redirect('/perfil');
+
     }
 
     /**
--- resources/views/pymes/perfil/editar-perfil.blade.php
+++ resources/views/pymes/perfil/editar-perfil.blade.php
@@ -12,37 +12,49 @@
                 <br>
 
                 <!-- ACÁ LLAMAMOS AL MÉTODO UPDATE QUE SIRVE PARA ACTUALIZAR LOS DATOS DEL PERFIL, Y LE PASAMOS LA ID DEL USUARIO -->
-                <form method="POST" action="{{action('App\Http\Controllers\PerfilController@update',$usuario->id)}}"> 
+                <form method="POST" action="{{action('App\Http\Controllers\PerfilController@update',$usuario->id)}}" enctype="multipart/form-data"> 
                     @csrf <!--PARA SEGURIDAD-->
                     @method('PATCH') <!-- ESTO SE COLOCA PARA QUE PUEDA INGRESAR LOS DATOS A LA BD, YA QUE NO ESTÁ DEFINIDO COMO POST EN EL WEB -->
+                    @if(isset($msg))
+                        {{$msg}}
+                    @endif
                     <div class="form-floating mb-3">
                         <label for="nombre">Nombre</label>
-                        <input type="text" class="form-control" id="nombre" value="{{$usuario->name}}" disabled>
+                        <input type="text" class="form-control" name="name" id="nombre" value="{{$usuario->name}}" disabled required>
                     </div>
                     
                     <div class="form-floating mb-3">
                         <label for="mail">Mail</label>
-                        <input type="text" class="form-control" id="mail" value="{{$usuario->email}}" disabled>
+                        <input type="text" class="form-control" name="mail" id="mail" value="{{$usuario->email}}" disabled required>
                     </div>
 
                     <div class="form-floating mb-3">
                         <label for="direccion">Dirección</label>
-                        <input type="text" class="form-control" id="direccion" value="{{$usuario->direccion}}" disabled>
+                        <input type="text" class="form-control" name="direccion" id="direccion" value="{{$usuario->direccion}}" disabled>
                     </div>
 
                     <div class="form-floating mb-3">
                         <label for="telefono">Teléfono</label>
-                        <input type="text" class="form-control" id="telefono" value="{{$usuario->telefono}}" disabled>
+                        <div class="row">
+                            <div class="col-1">
+                                <input type="number" class="form-control" placeholder="+569" disabled>
+                            </div>
+                            <div class="col-11">
+                                <input type="number" class="form-control" name="telefono" id="telefono" value="{{$usuario->telefono}}" disabled>
+                            </div>
+                        </div>
+                        
+                        
                     </div>
 
                     <div class="form-floating mb-3">
                         <label for="instagram">Instagram</label>
-                        <input type="text" class="form-control" id="instagram" value="{{$usuario->instagram}}" disabled>
+                        <input type="text" class="form-control" name="text" id="instagram" value="{{$usuario->instagram}}" disabled>
                     </div>
 
                     <div class="form-floating mb-3">
                         <label for="facebook">Facebook</label>
-                        <input type="text" class="form-control" id="facebook" value="{{$usuario->facebook}}" disabled>
+                        <input type="text" class="form-control" name="facebook" id="facebook" value="{{$usuario->facebook}}" disabled>
                     </div>
 
                     <hr>
@@ -50,11 +62,11 @@
                         <label for="imagen">Imagen de Perfil</label>
 
                         @if(isset($usuario->imagen_perfil)) <!-- SI TIENE FOTO DE PERFIL LA MUESTRA-->
-                            <img src="/{{$usuario->imagen_perfil}}" class="img-thumbnail" alt="...">
+                            <img src="{{$usuario->imagen_perfil}}" class="img-thumbnail" alt="..." width="200" height="100">
                         @else <!-- SI NO TIENE FOTO DE PERFIL LE INFORMA -->
                             <p style="text-align:center; color:red;">Sin Imagen de Perfil... Aún!</p>
                         @endif
-                        <input type="file" class="form-control" id="imagen" value="{{$usuario->facebook}}" disabled>
+                        <input type="file" class="form-control" id="imagen" name="file" disabled>
                     </div>
                     <hr>
                     <button type="submit" class="btn btn-success" id="botonEditar" disabled>Cambiar Datos</button>
@@ -69,7 +81,6 @@
     function editarPerfil(){
         if(document.getElementById("nombre").disabled == true){ //SI EL ELEMENTO NO ESTÁ HABILITADO PARA EDITAR SE CAMBIAN TODOS A HABILITADOS
             document.getElementById("nombre").disabled = false;
-            document.getElementById("mail").disabled = false;
             document.getElementById("direccion").disabled = false;
             document.getElementById("telefono").disabled = false;
             document.getElementById("instagram").disabled = false;
@@ -80,7 +91,6 @@
         }
         else{ //LO CONTRARIO A LA FUNCIÓN ANTERIOR, SI ESTÁN HABILITADOS PARA EDITAR SE CAMBIAN A NO HABILITADOS, INCLUYENDO EL BOTÓN DE AL FINAL
             document.getElementById("nombre").disabled = true;
-            document.getElementById("mail").disabled = true;
             document.getElementById("direccion").disabled = true;
             document.getElementById("telefono").disabled = true;
             document.getElementById("instagram").disabled = true;
